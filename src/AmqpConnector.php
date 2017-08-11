25b93b3bedf236934b60e3d399fe4f4e2e3cb80d
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-08-11 16:23:31 +0300

    add ability to change delay strategy

--- src/AmqpConnector.php
+++ src/AmqpConnector.php
@@ -2,7 +2,9 @@
 
 namespace Enqueue\LaravelQueue;
 
+use Enqueue\AmqpTools\DelayStrategy;
 use Enqueue\AmqpTools\DelayStrategyAware;
+use Enqueue\AmqpTools\RabbitMqDelayPluginDelayStrategy;
 use Enqueue\AmqpTools\RabbitMqDlxDelayStrategy;
 use Interop\Amqp\AmqpContext;
 
@@ -12,14 +14,24 @@ class AmqpConnector extends Connector
     {
         $queue = parent::connect($config);
 
+        $config = array_replace(['delay_strategy' => 'rabbitmq_dlx'], $config);
+
+
+
         /** @var AmqpContext $amqpContext */
         $amqpContext = $queue->getPsrContext();
         if (false == $amqpContext instanceof AmqpContext) {
             throw new \LogicException(sprintf('The context must be instance of "%s" but got "%s"', AmqpContext::class, get_class($queue->getPsrContext())));
         }
 
-        if ($amqpContext instanceof DelayStrategyAware) {
+        if ($amqpContext instanceof DelayStrategyAware && 'rabbitmq_dlx' == $config['delay_strategy']) {
             $amqpContext->setDelayStrategy(new RabbitMqDlxDelayStrategy());
+        }
+        if ($amqpContext instanceof DelayStrategyAware && 'rabbitmq_delay_plugin' == $config['delay_strategy']) {
+            $amqpContext->setDelayStrategy(new RabbitMqDelayPluginDelayStrategy());
+        }
+        if ($amqpContext instanceof DelayStrategyAware && $config['delay_strategy'] instanceof DelayStrategy) {
+            $amqpContext->setDelayStrategy($config['delay_strategy']);
         }
 
         return $queue;
