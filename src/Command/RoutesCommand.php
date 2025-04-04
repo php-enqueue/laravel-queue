edd0653cdf4b0e6a7da438d8e30815ef9f1623a4
Xavier Seguí Beltrán <xavier.seguibeltran@metro-markets.de>
2025-04-04 12:23:26 +0200

    Add named parameter name to ConsumeCommand and RoutesCommand

--- src/Command/RoutesCommand.php
+++ src/Command/RoutesCommand.php
@@ -3,7 +3,9 @@ namespace Enqueue\LaravelQueue\Command;
 
 use Enqueue\SimpleClient\SimpleClient;
 use Enqueue\Symfony\Client\SimpleRoutesCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
 
+#[AsCommand(name: 'enqueue:routes')]
 class RoutesCommand extends SimpleRoutesCommand
 {
     public function __construct(SimpleClient $client)
