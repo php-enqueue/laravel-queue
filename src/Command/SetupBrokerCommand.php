145503efee53758b0a77ce74e745098499382c5d
Xavier Seguí Beltrán <xavier.seguibeltran@metro-markets.de>
2025-04-04 12:17:50 +0200

    Configure SetupBrokerCommand with AsCommand Attribute from Symfony

--- src/Command/SetupBrokerCommand.php
+++ src/Command/SetupBrokerCommand.php
@@ -3,7 +3,9 @@ namespace Enqueue\LaravelQueue\Command;
 
 use Enqueue\SimpleClient\SimpleClient;
 use Enqueue\Symfony\Client\SimpleSetupBrokerCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
 
+#[AsCommand('enqueue:setup-broker')]
 class SetupBrokerCommand extends SimpleSetupBrokerCommand
 {
     public function __construct(SimpleClient $client)
