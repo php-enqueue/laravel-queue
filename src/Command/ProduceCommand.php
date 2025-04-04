9284fdca550f482b7867e18edb10fe22ac07a442
Xavier Seguí Beltrán <xavier.seguibeltran@metro-markets.de>
2025-04-04 12:21:55 +0200

    Add named parameter name to ProduceCommand

--- src/Command/ProduceCommand.php
+++ src/Command/ProduceCommand.php
@@ -3,7 +3,9 @@ namespace Enqueue\LaravelQueue\Command;
 
 use Enqueue\SimpleClient\SimpleClient;
 use Enqueue\Symfony\Client\SimpleProduceCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
 
+#[AsCommand(name: 'enqueue:produce')]
 class ProduceCommand extends SimpleProduceCommand
 {
     public function __construct(SimpleClient $client)
